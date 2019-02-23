7d77c53cda95a6671a71620588e80044281fab94
Baron <lotirmon@hotmail.com>
2019-02-22 19:23:24 -0600

    idk

--- app/actions/terapeutaActions.js
+++ app/actions/terapeutaActions.js
@@ -1,7 +1,9 @@
 const Terapeuta = require("../models/Terapeutas");
+const User = require("../models/User");
+const Citas = require("../models/Citas");
 
 const createTerapeuta = (data) =>{
-    return User.create(data);
+    return Terapeuta.create(data);
 }
 
 const getUserByEmail = (email) =>{
@@ -16,8 +18,12 @@ const getAllAsignedUsers = () =>{
     return User.find({is_active: true}).select("-password").populate("posts");
 }
 
-const getDates = () => {
+const getCitas = () => {
+    return Citas
+}
 
+const createCita =(Cita)=>{
+    return Citas.create(data);
 }
 
 module.exports={
